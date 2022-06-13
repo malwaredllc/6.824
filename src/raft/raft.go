ba5ab6d3b9dda54d70272b4f4f6ee0ccef997b51
malwaredllc <30509968+malwaredllc@users.noreply.github.com>
2022-06-13 16:20:12 -0600

    read persist for raft

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -18,6 +18,7 @@ package raft
 //
 
 import (
+	"log"
 	"bytes"
 	"sync"
 	"sync/atomic"
@@ -109,19 +110,20 @@ func (rf *Raft) readPersist(data []byte) {
 	if data == nil || len(data) < 1 { // bootstrap without any state?
 		return
 	}
-	// Your code here (2C).
-	// Example:
-	// r := bytes.NewBuffer(data)
-	// d := labgob.NewDecoder(r)
-	// var xxx
-	// var yyy
-	// if d.Decode(&xxx) != nil ||
-	//    d.Decode(&yyy) != nil {
-	//   error...
-	// } else {
-	//   rf.xxx = xxx
-	//   rf.yyy = yyy
-	// }
+	r := bytes.NewBuffer(data)
+	d := labgob.NewDecoder(r)
+	var currentTerm int
+	var votedFor int
+	var persistedLog []string
+	if d.Decode(&currentTerm) != nil ||
+	   d.Decode(&votedFor) != nil ||
+	   d.Decode(&persistedLog) != nil {
+		log.Printf("readPersist failed")
+	} else {
+	  rf.currentTerm = currentTerm
+	  rf.votedFor = votedFor
+	  rf.log = persistedLog
+	}
 }
 
 
